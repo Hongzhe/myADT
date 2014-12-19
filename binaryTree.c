358c2aefc198688f4b14bcf65f77273a19b1b763
hongzhe <hongzhe@outlook.com>
2014-12-19 16:54:38 +0000

    add remove node operation and few support functions

--- binaryTree.c
+++ binaryTree.c
@@ -16,6 +16,11 @@ typedef enum {
     WRONG = 2
 } status;
 
+treeNode* search_left_greatest(treeNode* start);
+int is_leaf(treeNode *node);
+int left_or_right(treeNode *node);
+void replace_deleted_node(treeNode *target, treeNode *origin);
+
 treeNode* new_node(int key, char* value, treeNode* parent) 
 {
     treeNode* new = malloc(sizeof(*new));
@@ -53,6 +58,77 @@ status insert_node(treeNode *new, treeNode* p)
     return GOOD;
 }
 
+/** 
+ * Remove a node from binary search Tree. Three situation
+ * 1. the node is a leaf node.
+ * 2. the node has either left or right child.
+ * 3. the node has both left and right chiildren.
+ */
+void delete_node(treeNode *target) {
+    treeNode *parent = target->parent;
+    if(target->left == NULL && target->right==NULL) 
+        if(parent->value > target->value)
+            parent->left = NULL;
+        else
+            parent->right = NULL;
+    else if(target->left == NULL) { 
+        if(parent->key > target->key)
+            parent->left = target->right;
+        else
+            parent->right = target->right;
+    } else if(target->right == NULL){        //right child is null
+        if(parent->key < target->key)
+            parent->right = target->left;
+        else
+            parent->left = target->left; 
+    } else {                                //target node has two children.
+        treeNode* replacement = search_left_greatest(target);
+        replace_deleted_node(replacement, target);
+    }
+    free(target);
+}
+
+void replace_deleted_node(treeNode* replacement, treeNode *origin) {
+     if(left_or_right(origin)) {
+        origin->parent->left = replacement;
+    }
+    else { 
+       origin-> parent->right = replacement;
+    }
+    replacement->left = origin->left;
+    origin->left->parent = replacement;
+    replacement->right = origin->right;
+    origin->right->parent= replacement;
+}
+
+treeNode* search_left_greatest(treeNode* start) {
+    List* list = init_list();
+    treeNode *result;
+    insert_at_head(list, start->left);
+    while(list->size != 0) {
+        treeNode* node = (treeNode*) remove_at_tail(list)->value;
+        if(is_leaf(node)) {
+            result = node;
+            break;
+        }
+        insert_at_head(list, (void*)node->right);
+    }
+    free(list);
+    return result;
+}
+
+int left_or_right(treeNode* node) {
+    int result;
+    result = node->parent->key > node->key ? 1:0;
+    return result;
+}
+
+int is_leaf(treeNode *node) {
+    if(node->left ==NULL && node->right == NULL) 
+        return 1;
+    return 0;
+}
+
 void search(int key, treeNode* start, char** result) {
     List* list = init_list();
     insert_at_head(list, start);
@@ -73,7 +149,6 @@ void search(int key, treeNode* start, char** result) {
     }
     free(list);
 }
-
 
 void print_tree(treeNode *start)
 {
